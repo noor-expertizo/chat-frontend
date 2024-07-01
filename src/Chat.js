8c5b265f17fad82296abb20f3d36ae8fd5a1a8ce
noor-expertizo <noorulain@expertizo.pk>
2024-07-01 13:21:30 +0500

    first commit t

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:5000');
+// const socket = io('http://localhost:5000');
+const socket = io('https://chat-backend-omega-drab.vercel.app/');
 
 const Chat = () => {
     const [username, setUsername] = useState('');
