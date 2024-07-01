860472975fb8b237cc5989bc838974838b7ccb84
noor-expertizo <noorulain@expertizo.pk>
2024-07-01 12:58:58 +0500

    first commit

---
+++ src/Chat.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:5000');
+
+const Chat = () => {
+    const [username, setUsername] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        socket.on('init', (messages) => {
+            setMessages(messages);
+        });
+
+        socket.on('message', (message) => {
+            setMessages((prevMessages) => [...prevMessages, message]);
+        });
+
+        return () => {
+            socket.off('init');
+            socket.off('message');
+        };
+    }, []);
+
+    const handleSendMessage = () => {
+        socket.emit('message', { username, message });
+        setMessage('');
+    };
+
+    return (
+        <div>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder="Message"
+                    value={message}
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button onClick={handleSendMessage}>Send</button>
+            </div>
+            <div>
+                {messages.map((msg, index) => (
+                    <div key={index}>
+                        <strong>{msg.username}: </strong>{msg.message}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Chat;
